3401c21ffdd626c808595c66ed5d17af765ccb53
Libing523 <385717153@qq.com>
2019-11-08 15:51:40 +0800

    9.3 完全解耦

--- src/main/java/interfaces/classprocessor/Apply.java
+++ src/main/java/interfaces/classprocessor/Apply.java
@@ -6,12 +6,15 @@ import java.util.Arrays;
  * 
  * @ClassName: Apply
  * @Description: 9.3 完全解耦
+ * 策略设计模型：
+ *     创建一个能够根据所传递的参数对象的不同而具有不同行为的方法
  * @author lishuaibing
  * @date 2019年11月8日 下午2:48:27
  *
  */
 
 class Processor {
+	//相当于策略
 	public String name() {
 		return getClass().getSimpleName();
 	}
@@ -49,6 +52,7 @@ public class Apply {
 	public static String s = "Disagreement with beliefs is by definition incorrect";
 
 	public static void main(String[] args) {
+		//有三种不同的类型的策略应用到String s 上
 		process(new Upcase(), s);
 		process(new Downcase(), s);
 		process(new Splitter(), s);
