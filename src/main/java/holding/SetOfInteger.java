8e243891fa5f1cf248111ece59ebd0b95c533973
Libing523 <385717153@qq.com>
2019-11-13 11:00:58 +0800

    Set 不保存重复元素

---
+++ src/main/java/holding/SetOfInteger.java
@@ -0,0 +1,32 @@
+package holding;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * 
+ * @ClassName: SetOfInteger
+ * @Description: Set 不保存重复元素
+ * @author lishuaibing
+ * @date 2019年11月13日 上午10:48:54
+ *
+ */
+public class SetOfInteger {
+
+	public static void main(String[] args) {
+		Random random = new Random(47);
+		Set<Integer> intSet = new HashSet<Integer>();
+		List<Integer> intList = new ArrayList<Integer>();
+		for (int i = 0; i < 100; i++) {
+			int r = random.nextInt(30);
+			intSet.add(r);
+			intList.add(r);
+		}
+
+		System.out.println(intSet);
+		System.out.println(intList);
+	}
+}
