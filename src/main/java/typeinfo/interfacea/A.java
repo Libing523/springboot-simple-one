c2fbb4e56f9cc83cc9498ef06dfc42ab9cb3ff04
Libing523 <385717153@qq.com>
2019-11-25 16:11:12 +0800

    接口与类型信息

---
+++ src/main/java/typeinfo/interfacea/A.java
@@ -0,0 +1,14 @@
+package typeinfo.interfacea;
+
+/**
+ * 
+    * @ClassName: A
+    * @Description: 接口与类型信息
+    * @author lishuaibing
+    * @date 2019年11月25日 下午2:31:42
+    *
+ */
+public interface A {
+
+	void f();
+}
