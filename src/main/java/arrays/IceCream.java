3231560b2c03dc7b5976b092f9152e938a8075af
Libing523 <385717153@qq.com>
2019-11-28 16:42:40 +0800

    16.3 返回一个数组

---
+++ src/main/java/arrays/IceCream.java
@@ -0,0 +1,46 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 
+ * @ClassName: IceCream
+ * @Description: 16.3 返回一个数组
+ * @author lishuaibing
+ * @date 2019年11月28日 下午4:09:28
+ *
+ */
+public class IceCream {
+
+	private static Random rand = new Random(47);
+	static final String[] FLAVORS = { "C", "S", "VFS", "MC", "MAF", "RR", "PC", "MP"};
+	public static String[] flavorSet(int n) {
+		if(n>FLAVORS.length)
+			return null;
+		String[] results = new String[n];
+		boolean[] picked = new boolean[FLAVORS.length];
+		for(int i=0;i<n;i++) {
+			int t;
+			do
+				t = rand.nextInt(FLAVORS.length);
+			while(picked[t]);
+			results[i] = FLAVORS[t];
+			picked[t] = true;
+		}
+		
+		
+		return results;
+	}
+
+	
+	public static void main(String[] args) {
+		for(int i=0;i<7;i++)
+			System.out.println(Arrays.toString(flavorSet(3)));
+	}
+	
+	
+	
+	
+	
+}
