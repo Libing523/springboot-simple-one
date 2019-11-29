466baf50ae9164f14044b9ecf91080c8dd8573d1
Libing523 <385717153@qq.com>
2019-11-29 09:10:44 +0800

    16.4 多维数组

---
+++ src/main/java/arrays/MultidimensionalPrimitiveArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @ClassName: MultidimensionalPrimitiveArray
+ * @Description: 16.4 多维数组
+ * @author lishuaibing
+ * @date 2019年11月29日 上午8:58:40
+ *
+ */
+public class MultidimensionalPrimitiveArray {
+
+	public static void main(String[] args) {
+		int[][] a = { { 1, 2, 3 }, { 4, 5, 6 } };
+		System.out.println(Arrays.deepToString(a));
+	}
+}
