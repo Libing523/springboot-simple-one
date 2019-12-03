280a77203479189f132c1029a8c859cbe7c267b9
Libing523 <385717153@qq.com>
2019-12-03 14:48:26 +0800

    16.7.3 数组元素的比较

---
+++ src/main/java/arrays/CompType.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import java.util.Random;
+
+import net.mindview.util.Generator;
+
+/**
+ * @ClassName: CompType
+ * @Description: 16.7.3 数组元素的比较
+ * @author lishuaibing
+ * @date 2019年12月3日 下午2:17:32
+ *
+ */
+public class CompType implements Comparable<CompType> {
+	int i;
+	int j;
+
+	private static int count = 1;
+
+	public CompType(int n1, int n2) {
+		i = n1;
+		j = n2;
+	}
+
+	public String toString() {
+		String result = "{i = " + i + ",j = " + j + "}";
+		if (count++ % 3 == 0)
+			result += "\n";
+		return result;
+	}
+
+	@Override
+	public int compareTo(CompType rv) {
+		return (i < rv.i ? -1 : (i == rv.i ? 0 : 1));
+	}
+
+	private static Random r = new Random(47);
+
+	public static Generator<CompType> generator() {
+		return new Generator<CompType>() {
+			@Override
+			public CompType next() {
+				return new CompType(r.nextInt(100), r.nextInt(100));
+			}
+		};
+	}
+	
+
+}
