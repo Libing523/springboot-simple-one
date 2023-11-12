0c084fbe91693a6a4f8acf6c722a5979632226a6
jinjin~er <3852717153@qq.com>
2023-11-12 16:50:45 +0800

    我修改了东西2222

--- src/main/java/com/bing/springboot_simple_one/controller/simpleController.java
+++ src/main/java/com/bing/springboot_simple_one/controller/simpleController.java
@@ -11,6 +11,7 @@ public class simpleController {
 	public String hello() {
 		System.out.println("11111");
 		System.out.println("22222");
+		System.out.println("验证冲突");
 
 		return "helloworld2";
 	}
